840ae8a76f6ef0136c0865ea1bdf684b2a8b657b
kevinmarkman <kevin.w.markman@gmail.com>
2023-03-31 20:07:49 -0500

    adds function to draw blocks and timer to make blocks fall

--- phase-1-project/script.js
+++ phase-1-project/script.js
@@ -29,9 +29,9 @@ document.addEventListener('DOMContentLoaded',() => {
     ];
 
     const tetrominoO = [
-        [0,1, width, width+1]
-        [0,1, width, width+1],
-        [0,1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
         [0, 1, width, width+1],
     ];
 
@@ -41,26 +41,73 @@ document.addEventListener('DOMContentLoaded',() => {
         [1,width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3],
     ];
-    })
 
-    const theTetrominoes = [tetrominoL, tetrominoZ, tetrominoT, tetrominoO, tetrominoI];
+    const theTetrominoes = [tetrominoI,tetrominoL,tetrominoO,tetrominoT,tetrominoZ];
     let currentPosition = 4;
 
     //Randomly select a tetromino at its first rotation 
     let randomShape = Math.floor(Math.random()*theTetrominoes.length);
     let currentRotation = 0;
-    let currentShape = theTetrominoes[randomShape][currentRotation]
+    let currentShape = theTetrominoes[randomShape][currentRotation];
+
 
     //draw the tetromino
     function draw(){
-        current.forEach(index => {
-            squares[currentPosition + index].classList.add('tetromino')
+        currentShape.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
         })
     }
 
+    draw();
+
     //undraw the tertomino
     function undraw(){
-        current.forEach(index => {
+        currentShape.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
         })
     }
+
+    //tetrominoes downward speed 1000 = 1 sec
+    let timerValue = 1000;
+    let timerId = setInterval(moveDown,timerValue);
+
+
+
+    document.addEventListener('onkeypress',control);
+
+    //moves the tetrominoes down based on the above speed
+    function moveDown(){
+        undraw();
+        currentPosition += width
+        draw();
+        freezeBlock();
+    }
+    
+    //freeze block
+    function freezeBlock(){
+        if(currentShape.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            currentShape.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //Increases the falling speed 
+            timerValue = timerValue - 10;
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            currentShape = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        }
+    }
+
+    //move tetromino left unless blocked by another block or edge
+    function moveLeft(){
+        undraw();
+        const atLeftEdge = currentShape.some(index => (currentPosition + index) % width === 0);
+        if(!atLeftEdge)currentPosition -= 1;
+        if(currentPosition.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1;
+        }
+    }
+
+
+
+//DOM listener close
+})
