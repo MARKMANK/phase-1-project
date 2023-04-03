2c164aee10c8e9ba3554609180db5a9cf15a5afd
kevinmarkman <kevin.w.markman@gmail.com>
2023-04-03 18:41:02 -0500

    fixes rotation check and adds delay before a piece settles

--- phase-1-project/script.js
+++ phase-1-project/script.js
@@ -123,12 +123,15 @@ document.addEventListener('DOMContentLoaded',() => {
 
     //moves the tetrominoes down
     function moveDown() {
+        if(!current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
         undraw()
         currentPosition += width
         draw()
-        freezeBlock()
+        } else {
+        freezeBlock();
       }
-    
+    }
+
     //freeze block
     function freezeBlock(){
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
@@ -166,6 +169,30 @@ document.addEventListener('DOMContentLoaded',() => {
             currentPosition -= 1;
         }
         draw();
+      }
+    ///FIX ROTATION OF TETROMINOS A THE EDGE 
+    function isAtRight() {
+        return current.some(index=> (currentPosition + index + 1) % width === 0)  
+    }
+    
+    function isAtLeft() {
+        return current.some(index=> (currentPosition + index) % width === 0)
+    }
+
+    function checkRotatedPosition(P){
+        P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
+        if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
+          if (isAtRight()){            //use actual position to check if it's flipped over to right side
+            currentPosition += 1    //if so, add one to wrap it back around
+            checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+            }
+        }
+        else if (P % width > 5) {
+          if (isAtLeft()){
+            currentPosition -= 1
+          checkRotatedPosition(P)
+          }
+        }
       }
 
     //rotate the tetromino
