de5eed2104695f81a8231fb49dde4549088fa730
kevinmarkman <kevin.w.markman@gmail.com>
2023-04-03 22:03:30 -0500

    fixes unNext issue

--- phase-1-project/script.js
+++ phase-1-project/script.js
@@ -20,39 +20,39 @@ document.addEventListener('DOMContentLoaded',() => {
     //The Tetrominoes
 
     const tetrominoL = [
-        [1,width+1, width*2+1,2],
+        [1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
         [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1, width*2+2],
-    ];
+        [width, width*2, width*2+1, width*2+2]
+    ]
 
     const tetrominoZ = [
-        [0, width, width+1, width*2+1],
+        [0, width, width+1,width*2+1],
         [width+1, width+2, width*2, width*2+1],
         [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1],
-    ];
+        [width+1, width+2, width*2, width*2+1]
+    ]
 
     const tetrominoT = [
         [1, width, width+1, width+2],
-        [1,width+1, width+2, width*2+1],
+        [1, width+1, width+2, width*2+1],
         [width, width+1, width+2, width*2+1],
         [1, width, width+1, width*2+1]
-    ];
+    ]
 
     const tetrominoO = [
         [0, 1, width, width+1],
         [0, 1, width, width+1],
         [0, 1, width, width+1],
-        [0, 1, width, width+1],
-    ];
+        [0, 1, width, width+1]
+    ]
 
     const tetrominoI = [
         [1, width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3],
         [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3],
-    ];
+        [width, width+1, width+2, width+3]
+    ]
 
     const tertominoReverseL = [
         [0, 1, width+1, width*2+1],
@@ -62,11 +62,12 @@ document.addEventListener('DOMContentLoaded',() => {
     ];
 
     const tertominoReverseZ = [
-        [width, width+1, width*2+1,width*2+2],
+        [width, width+1, width*2+1, width*2+2],
         [1, width+1, width, width*2],
-        [width, width+1, width*2+1,width*2+2],
+        [width, width+1, width*2+1, width*2+2],
         [1, width+1, width, width*2],
     ];
+      
 
     const tertominoX = [
         [1, width, width+1, width+2, width*2+1],
@@ -75,7 +76,7 @@ document.addEventListener('DOMContentLoaded',() => {
         [1, width, width+1, width+2, width*2+1],
     ];
 
-    const theTetrominoes = [tetrominoI,tetrominoL,tetrominoO,tetrominoT,tetrominoZ,tertominoReverseL,tertominoReverseZ,tertominoX];
+    const theTetrominoes = [tertominoReverseL, tertominoReverseZ, tertominoX, tetrominoI, tetrominoL, tetrominoO, tetrominoT, tetrominoZ];
     let currentPosition = 4;
     let currentRotation = 0;
  
@@ -229,7 +230,7 @@ document.addEventListener('DOMContentLoaded',() => {
             square.style.backgroundColor = ''
         })
         upNext[nextRandom].forEach(index => {
-            displaySquares[displayIndex + index].classList.add('tertromino')
+            displaySquares[displayIndex + index].classList.add('tetromino')
             displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
         })
     }
